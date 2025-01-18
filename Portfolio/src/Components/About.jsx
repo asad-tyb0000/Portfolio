48093deda87c1f12d6ab63337b45aa1307bc1f69
asad-tyb0000 <asad.tyb0000@gmail.com>
2025-01-18 19:40:36 +0500

    some fixes

--- Portfolio/src/Components/About.jsx
+++ Portfolio/src/Components/About.jsx
@@ -5,27 +5,27 @@ function About() {
   return (
     <div id="about" className="aboutsection">
       <div className="flexcont">
-        <div className="Headingmain">
-          <div>
-            Failure is the seasoning
-            <br /> that enriches <span className="highlight">success</span>
-          </div>
-          <div className="aboutText">
-            As a dedicated Software Engineer and Web Developer, I focus on{" "}
-            <br />
-            building innovative, efficient, and user-focused solutions,
-            transforming
-            <br /> challenges into opportunities to create impactful digital
-            experiences
-          </div>
-        </div>
-
         <div className="button-container">
           <button className="cvButton">
             Download CV
             <img src="/icon2.png" alt="Next" className="buttonImage" />
           </button>
         </div>
+        <div className="Headingmain">
+          <div>
+            Failure is the seasoning
+            <br /> that enriches <span className="highlight">success</span>
+          </div>
+          <div className="aboutText">
+            &nbsp; As a dedicated Software Engineer and Web Developer, I focus on{" "}
+            <br />
+            &nbsp;&nbsp;building innovative, efficient, and user-focused solutions,
+            transforming
+            <br />&nbsp; challenges into opportunities to create impactful digital
+            experiences
+          </div>
+        </div>
+
       </div>
         <div className="emptyGap"></div>
     </div>
